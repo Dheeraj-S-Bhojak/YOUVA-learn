5138c84bcd3768f12800fbb894f212debfbe5387
Dheeraj Bhojak <dheerajsbhojakwork@gmail.com>
2023-01-26 16:00:36 +0530

    css error fixed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import NavBar from "./components/component/navBar.component.tsx";
-import Home from "./components/home/home.component.tsx";
 
 const App = () => {
   return (
     <main>
       <Routes>
-        <Route path='/' element={<NavBar />}>
-          <Route index element={<Home />} />
-        </Route>
+        <Route path='/' element={<NavBar />}></Route>
       </Routes>
     </main>
   );
