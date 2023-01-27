15d791ba02301d75e85f2ca989dd397d7c93e037
nick-509 <>
2023-01-27 16:47:47 +0530

    Banner form implemented

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import { Route, Routes } from "react-router-dom";
 import BannerForm from "./components/banner-form/banner-form.tsx";
 import NavBar from "./components/navbar/navBar.component.tsx";
+import Banner from './components/banner/banner.component.tsx';
 
 const App = () => {
   return (
     <main>
-      <Routes>
-        <Route path='/' element={<NavBar />}>
-          <Route index element={<Banner />} />
-        </Route>
-      </Routes>
+      <NavBar />
+      <BannerForm />
+      <Banner />
     </main>
   );
 };
