134b1c42e93b30707150e536d7909c6a54e47022
nick-509 <gauravsewag32@gmail.com>
2023-02-01 18:13:38 +0530

    about-us-page-added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Banner from "./components/banner/banner.component.tsx";
 import Footer from "./components/footer/footer.component.tsx";
 import NavBar from "./components/navbar/navBar.component.tsx";
+import Testimonials from "./components/testimonials/testimonials.component.tsx";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <NavBar />
       <Routes>
         <Route path="/" element={<Banner />} />
+        <Route path="/about" element={<Testimonials />} />
       </Routes>
       <Footer />
     </main>
