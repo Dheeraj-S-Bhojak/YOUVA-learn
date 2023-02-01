fd337897fa1c8ad854fe8221ff572740cad0d83c
Dheeraj Bhojak <dheerajsbhojakwork@gmail.com>
2023-02-01 15:38:23 +0530

    contact-us component add

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Banner from "./components/banner/banner.component";
 import Footer from "./components/footer/footer.component";
 import NavBar from "./components/navbar/navBar.component";
+import Support from "./components/support/support.component";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <NavBar />
       <Routes>
         <Route path="/" element={<Banner />} />
+        <Route path="/contact-us" element={<Support />} />
       </Routes>
       <Footer />
     </main>
