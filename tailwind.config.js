7c408bda7fe483d72881deff18a0b2136c68bad7
Dheeraj Bhojak <dheerajsbhojakwork@gmail.com>
2023-02-02 16:32:51 +0530

    tailwind solved

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/components/**/*.{tsx}",
+    "./src/index.tsx",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
